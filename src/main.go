c537ea1858eaa3ad76146a2a5de50db555dda8c8
Salehalobilan <salehwleed1@gmail.com>
2025-08-05 22:41:12 +0300

    setting up main server file and enhance routing system

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "content-management-system/src/models"
+    // "content-management-system/src/models" // needs it for automigrate
     "content-management-system/src/routes"
     "content-management-system/src/utils"
 
@@ -11,7 +11,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/gorm"
 )
+
+func SetupRoutes(router *gin.Engine, db *gorm.DB) {
+	routes.SetupPostRoutes(router, db)
+	routes.SetupMediaRoutes(router, db)
+	routes.SetupPageRoutes(router, db)
+}
 
 func main() {
 
@@ -20,17 +27,22 @@ func main() {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	utils.AutoMigrate(db) // new
 
 
 
-	utils.LoadEnv() // new
+
 	env := os.Getenv("ENV")
+	if env == "" { //if env is not set, set it to development as default
+		env = "development"
+	}
+
+	utils.AutoMigrate(db) // use it in development 
 
 
 	router := gin.Default()
 
-	routes.SetupRoutes(router, db)
+	
+	SetupRoutes(router, db)
 
 
 
